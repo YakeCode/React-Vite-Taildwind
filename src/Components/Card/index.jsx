e7b1581fcf0d35cacd50f2706794bac32ad34acf
YakeCode <y.gaviriaescobar@gmail.com>
2024-09-15 22:49:41 -0500

    consumiendo una API he incorporandola al proyecto

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,17 +1,19 @@
-const Card = ()=> {
+const Card = (data)=> {
+
+    
     return (
 
-        <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
+        <div  id={data.data.id} className=" bg-white cursor-pointer w-60 h-64 rounded-lg mb-4">
 
             <figure className="relative mb-2 w-full h-4/5 ">
 
                 <span className="absolute left-0 bottom-0 bg-white/60 rounded-md text-xs px-3 py-0.5 text-black m-2 "
                 >
-                    Category
+                    {data.data.category}
                 </span>
 
-                <img className="w-full h-full object-cover rounded-lg"
-                src="https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=600" alt="product-img" />
+                <img className="w-full h-full object-fill rounded-lg"
+                src={data.data.image} alt={data.data.title + 'img'}  />
 
                 <button className="absolute top-0 right-0 flex justify-center items-center rounded-full bg-white/90 text-black/70 w-6 h-6 m-2 p-1 font-extrabold text-lg">
                     +
@@ -19,8 +21,13 @@ const Card = ()=> {
 
             </figure>
             <p className="flex justify-between items-center w-full">
-                <span className="text-md font-light">Product</span>
-                <span className="font-semibold text-lg">$Price</span>
+                <span className="text-sm font-light w-3/5">
+                    {data.data.title}
+                </span>
+
+                <span className="font-semibold text-lg w-1/5">
+                    ${data.data.price}
+                </span>
             </p>
         </div>
     )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,35 @@
 //import React from 'react'
+import { useState, useEffect } from "react"
 import Card from "../../Components/Card"
 import { Layout } from "../../Components/Layout"
 
 function Home() {
+
+    const [items, setItems] = useState (null)
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(response=>response.json())
+            .then(data=>setItems(data))
+            .catch((error)=>{
+                console.log(error)
+            })
+    },[])
+
     return (
         <Layout>
-            <Card/>
-        </Layout>
+            <div className=" w-full grid gap-5 grid-cols-3 max-w-screen-lg" >
+
+            {
+                items?.map( (item)=> (
+                    <Card 
+                        key = {item.id}
+                        data = {item}
+                    />
+                ))
+            }
+            </div>
+        </Layout> 
         
     )
 }
